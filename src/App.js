6213fbe984750d4e10b0a0d578d7f9186836def7
angelks <angelks4@live.com>
2019-01-13 22:40:06 +0800

    feat: add loaders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./index.css";
+import img from "./assets/avatar.jpeg";
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <div className="text">
+      hello
+      <img src={img} />
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -7,13 +8,34 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
+    alias: {
+      Utils: path.resolve(__dirname, "src/utils/"),
+      Compo: path.resolve(__dirname, "src/components/")
+    },
     extensions: ["*", ".js", ".jsx"]
   },
   output: {
@@ -25,7 +47,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html")
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin([path.join(__dirname, "dist")])
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
