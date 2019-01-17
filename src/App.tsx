ce2491aa722678f16a8d41f893283f9d8c7b88aa
Zarks Chow <zhouzeze@decobim.com>
2019-01-17 13:35:18 +0800

    🔧: add typings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { DefaultButton } from "office-ui-fabric-react/lib/Button";
-// import "./index.module.css";
-import styles from "./index.module.css";
+import * as styles from "./index.module.css";
+import * as avatar from "./assets/avatar.jpeg";
 export interface HelloProps {
   compiler: string;
   framework: string;
@@ -11,13 +11,12 @@ export class Hello extends React.Component<HelloProps, {}> {
   render() {
     return (
       <div>
-        {/* <h1 className="text"> */}
         <h1 className={styles.text}>
           Hello from {this.props.compiler} and {this.props.framework}!
         </h1>
         <DefaultButton>I am a button.</DefaultButton>
+        <img src={avatar} />
       </div>
     );
   }
 }
-// "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
