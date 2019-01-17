1d0efc49ecb8d99c79801c9852b0f1db8feff1f4
angelks <angelks4@live.com>
2019-01-17 23:56:04 +0800

    add preview fluent designn

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { DefaultButton } from "office-ui-fabric-react/lib/Button";
 import * as styles from "./index.module.css";
 import * as avatar from "./assets/avatar.jpeg";
+import { Customizer } from "office-ui-fabric-react";
+import Nav from "./components/Nav";
+import { FluentCustomizations } from "@uifabric/fluent-theme";
 export interface HelloProps {
   compiler: string;
   framework: string;
@@ -10,13 +13,16 @@ export interface HelloProps {
 export class Hello extends React.Component<HelloProps, {}> {
   render() {
     return (
-      <div>
-        <h1 className={styles.text}>
-          Hello from {this.props.compiler} and {this.props.framework}!
-        </h1>
-        <DefaultButton>I am a button.</DefaultButton>
-        <img src={avatar} />
-      </div>
+      <Customizer {...FluentCustomizations}>
+        <Nav />
+        <div>
+          <h1 className={styles.text}>
+            Hello from {this.props.compiler} and {this.props.framework}!
+          </h1>
+          <DefaultButton>I am a button.</DefaultButton>
+          <img src={avatar} />
+        </div>
+      </Customizer>
     );
   }
 }
