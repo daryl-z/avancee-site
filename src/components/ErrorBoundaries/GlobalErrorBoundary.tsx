4bfb677a7fc4564db60e2d6698f81b58f053dc18
angelks <angelks4@live.com>
2019-01-19 11:50:38 +0800

    style: don't write anyscript

--- src/components/ErrorBoundaries/GlobalErrorBoundary.tsx
+++ src/components/ErrorBoundaries/GlobalErrorBoundary.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
 
 interface IState {
-  readonly error: any;
-  readonly errorInfo: any;
+  readonly error: Error | null;
+  readonly errorInfo: React.ErrorInfo;
 }
 
-export default class GlobalErrorBoundary extends React.Component<any, any> {
-  constructor(props: any) {
+export interface IProps {
+  children?: React.ReactNode;
+}
+export default class GlobalErrorBoundary extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
     super(props);
     this.state = { error: null, errorInfo: null };
   }
-  public componentDidCatch(error: any, errorInfo: any): void {
+  public componentDidCatch(error: Error | null, errorInfo: React.ErrorInfo): void {
     this.setState({
       error: error,
       errorInfo: errorInfo
     });
   }
 
-  public render(): any {
+  public render(): React.ReactNode {
+    const { children } = this.props;
     if (this.state.errorInfo) {
       return (
         <div>
@@ -30,6 +34,6 @@ export default class GlobalErrorBoundary extends React.Component<any, any> {
         </div>
       );
     }
-    return this.props.children;
+    return children;
   }
 }
