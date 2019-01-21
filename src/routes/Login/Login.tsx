d693018b1b4abc805dcf5a1dfb59dafdeb026a96
Edward <3zangelks@gmail.com>
2019-01-21 13:42:09 +0800

    ➕: login page

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { css, classNamesFunction } from 'office-ui-fabric-react/lib/Utilities';
+import { Label } from 'office-ui-fabric-react/lib/Label';
 import { getStyles, ILoginProps, ILoginStyles } from './Login.styles';
 import { Formik, Form, Field, ErrorMessage, FormikProps } from 'formik';
+import { Link } from 'react-router-dom';
 import * as styles from './index.m.scss';
-import { string } from 'prop-types';
 
 interface IError {
   email?: string;
@@ -19,42 +21,57 @@ const Login: React.FunctionComponent = props => {
   return (
     <div>
       <div className={css(classNames.login)}>
-        <h1 className={styles.logo}>Logo</h1>
+        <div className={styles.header}>
+          <a className={styles.logo}>
+            Logo
+            {/*  TODO: img */}
+          </a>
+        </div>
         <div className={styles.loginCard}>
-          <h1>Log in</h1>
-          <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
-              let errors: IError = {};
-              if (!values.email) {
-                errors.email = 'Required';
-              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                errors.email = 'Invalid email address';
-              }
-              return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" />
-                <Field type="password" name="password" />
-                <ErrorMessage name="password" component="div" />
-                {/* <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button> */}
-                <PrimaryButton type="submit" disabled={isSubmitting}>
-                  Login
-                </PrimaryButton>
-              </Form>
-            )}
-          </Formik>
+          <div className={styles.loginContent}>
+            <h1 className={styles.title}>Log In</h1>
+            <div className={styles.inner}>
+              <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={values => {
+                  let errors: IError = {};
+                  if (!values.email) {
+                    errors.email = 'Required';
+                  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                    errors.email = 'Invalid email address';
+                  }
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                  }, 400);
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <div className="formItem">
+                      <Label htmlFor="anInput">Email Address</Label>
+                      <TextField type="email" name="email" required={true} placeholder="Email" />
+                    </div>
+                    <div className="formItem">
+                      <Label htmlFor="anInput">Password</Label>
+                      <TextField type="password" name="password" required={true} placeholder="Password" />
+                    </div>
+                    <div className={styles.inputHelp}>
+                      <Link to="#">Forget your password</Link>
+                    </div>
+                    <div className="btnWrapper">
+                      <PrimaryButton type="submit" disabled={isSubmitting}>
+                        Login
+                      </PrimaryButton>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
         </div>
       </div>
     </div>
