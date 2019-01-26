612ce8d884c0c69c01510ef086343044ab4d45c1
angelks <angelks4@live.com>
2019-01-26 23:46:34 +0800

    add : routelist render

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import * as React from 'react';
-import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import { loadTheme } from 'office-ui-fabric-react';
 import GlobalErrorBoundary from './components/ErrorBoundaries/GlobalErrorBoundary';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { defaultTheme } from './utils/themes';
-import AsyncCompo from './routes';
+import routeList from './routes';
+import { IRoute, IChild } from './routes/index.d';
 
 loadTheme({
   palette: defaultTheme
 });
-
 const App: React.FunctionComponent = () => {
   return (
     // preview fluent ui
     // <Customizer {...FluentCustomizations}>
     <Router>
       <GlobalErrorBoundary>
-        {/* <Link to="/">
+        <Link to="/">
           <PrimaryButton>Home</PrimaryButton>
         </Link>
-        <Link to="/login">
+        <Link to="/auth/login">
           <PrimaryButton>Login</PrimaryButton>
         </Link>
-        <Link to="/About">
+        <Link to="/about">
           <PrimaryButton>About</PrimaryButton>
-        </Link> */}
+        </Link>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
-            <Route exact path="/" render={() => <AsyncCompo componentName="Home" />} />
-            <Route path="/about" render={() => <AsyncCompo componentName="About" />} />
-            <Route path="/login" render={() => <AsyncCompo componentName="Login" />} />
+            {routeList.map((item: IRoute) =>
+              item.children.map((child: IChild) => {
+                return <Route key={child.path} path={`/${item.path}/${child.path}`} component={child.component} />;
+              })
+            )}
+            <Redirect from="/" to="/home/index" exact />
           </Switch>
         </Suspense>
       </GlobalErrorBoundary>
