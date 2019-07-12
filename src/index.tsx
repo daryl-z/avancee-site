f6b028b9f0c6a0aad6ede2651862ae23d5b7dc63
Edward <3zangelks@gmail.com>
2019-07-12 15:47:56 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { cube } from './math';
+import { cube } from './math'; // 不能只shaking掉使用 shaking掉那个文件中的其他的export
 
 import './index.css';
 import App from './App';
@@ -8,7 +8,7 @@ import App from './App';
 function TestCompo(): any {
   return <div>{['Hello webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n')}</div>;
 }
-ReactDOM.render(<TestCompo />, document.getElementById('root'));
-// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<TestCompo />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 module.hot.accept();
